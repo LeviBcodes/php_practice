37feb84f2696e732ccec79f97c21487bedeecb9f
LeviBcodes <90584200+LeviBcodes@users.noreply.github.com>
2022-09-24 12:26:54 -0600

    functions

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+//arguments go into the function when created.
+    function registerUser($email) {
+        echo $email . ' registered';
+    }
+//parameters are passed into the function when called.
+registerUser('levi@mail.com');
+
